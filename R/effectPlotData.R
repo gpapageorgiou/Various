da24d1a71608674a9e1a927a33f09d5d8cc93b7a
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-06-20 10:35:44 +0200

    Effect plot data nlme

---
+++ R/effectPlotData.R
@@ -0,0 +1,22 @@
+##################################################################
+# effectPlotData (by D. Rizopoulos Repeated Measurements Course)
+#####################################################################
+
+effectPlotData <- function (object, newdata, orig_data) {
+  form <- formula(object)
+  namesVars <- all.vars(form)
+  betas <- if (!inherits(object, "lme")) coef(object) else fixef(object)
+  V <- if (inherits(object, "geeglm")) object$geese$vbeta else vcov(object)
+  orig_data <- orig_data[complete.cases(orig_data[namesVars]), ]
+  Terms <- delete.response(terms(form))
+  mfX <- model.frame(Terms, data = orig_data)
+  Terms_new <- attr(mfX, "terms")
+  mfX_new <- model.frame(Terms_new, newdata, xlev = .getXlevels(Terms, mfX))
+  X <- model.matrix(Terms_new, mfX_new)
+  pred <- c(X %*% betas)
+  ses <- sqrt(diag(X %*% V %*% t(X)))
+  newdata$pred <- pred
+  newdata$low <- pred - 1.96 * ses
+  newdata$upp <- pred + 1.96 * ses
+  newdata
+}
