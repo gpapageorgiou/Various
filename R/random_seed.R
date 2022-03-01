59c3d120551e93fe52807633b4a60974f47242de
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2022-03-01 09:56:27 +0100

    global attach

--- R/random_seed.R
+++ R/random_seed.R
@@ -1,7 +1,7 @@
 random_seed <- function() {
   start_seed <- Sys.time()
   start_seed <- as.integer(start_seed)
-  start_seed <- round(start_seed / 10, digits = 0)
+  start_seed <<- round(start_seed / 10, digits = 0)
   start_seed
 }
 
